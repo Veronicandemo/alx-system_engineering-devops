41867c8b3f67091e919eaeac41999155612e1449
Veronicandemo <ndemoveronica@gmail.com>
2022-08-24 00:40:46 +0300

    Regular Expressions

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #This Script creates a Ruby script that accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/[hbtn]/).join
+puts ARGV[0].scan(/hbo?t(1,4)n/).join
