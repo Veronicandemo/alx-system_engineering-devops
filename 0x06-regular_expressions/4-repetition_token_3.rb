8c4864bc21f3777df72676da9b6a5c48f8313426
Veronicandemo <ndemoveronica@gmail.com>
2022-08-24 01:10:39 +0300

    Regular expression

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #This Script creates a Ruby script that accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/hbn|hb?t{1,4}n/).join
+puts ARGV[0].scan(/hbn|hbt{1,4}n/).join
